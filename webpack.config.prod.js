9760a37764d12c16c571d4a373a4bab4768d942f
Maxwel Dsouza <maxellusionist@gmail.com>
2017-02-23 21:45:42 +0530

    Append hashes to assets

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,10 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].[contenthash].css',
+    filename: 'css/[name].[hash].css',
     disable: false,
 });
 
@@ -36,10 +37,12 @@ module.exports = {
         ],
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: '/assets/',
+        filename: 'js/bundle.[hash].js',
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new webpack.optimize.UglifyJsPlugin({
             beautify: false,
             mangle: {
@@ -55,6 +58,7 @@ module.exports = {
             // https://github.com/kangax/html-minifier#options-quick-reference
             title: 'My App',
             template: 'src/index.html',
+            filename: '../index.html',
             minify: {
                 collapseWhitespace: true,
                 minifyCSS: true,
