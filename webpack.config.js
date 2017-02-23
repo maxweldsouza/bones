8e405cbabf34ddba6ace019793ecf2e0aa9ba7c5
Maxwel Dsouza <maxellusionist@gmail.com>
2017-02-23 19:25:43 +0530

    Dev server with hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,10 +9,30 @@ module.exports = {
             {
                 test: /\.js$/, use: 'babel-loader',
             },
+            {
+                test: /\.scss$/, use: [{
+                    loader: 'style-loader', // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS
+                }, {
+                    loader: 'sass-loader', // compiles Sass to CSS
+                }],
+            },
         ],
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dev'),
         filename: 'bundle.js',
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dev'),
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: 'public/index.html',
+        }),
+    ],
 };
