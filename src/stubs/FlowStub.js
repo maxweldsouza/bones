bda1a438ebcfd9e10110806872bc633bbba99e78
Maxwel D'souza <maxellusionist@gmail.com>
2017-02-25 13:00:29 +0530

    Cleanup flow sub

--- src/stubs/FlowStub.js
+++ src/stubs/FlowStub.js
@@ -1,4 +1,6 @@
 // @flow
-type CSSModule = { [key: string]: string }
-const emptyCSSModule: CSSModule = {}
-export default emptyCSSModule
+// Avoid Required module not found warnings
+// https://stackoverflow.com/questions/36912675/flow-required-module-not-found/37272883#37272883
+type Module = { [key: string]: string }
+const emptyModule: Module = {}
+export default emptyModule
