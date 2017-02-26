4e14d60b2c1ffa4f73b7b5babcf631e453476551
Maxwel Dsouza <maxellusionist@gmail.com>
2017-02-26 23:44:15 +0530

    Added react router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import reducer from './reducers';
 import App from './components/App';
 import './index.scss';
@@ -10,7 +11,9 @@ const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path='/' component={App} />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
