8bd4db745401536539b1fe288291c30bd06326f8
Maxwel Dsouza <maxellusionist@gmail.com>
2017-03-12 21:01:57 +0530

    Add doiuse

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,18 @@
 module.exports = {
     plugins: [
         require('precss'),
-        require('stylelint'),
         require('autoprefixer'),
+        require('doiuse')({
+            browsers: [
+                'last 2 versions'
+            ],
+            ignore: ['rem'], // an optional array of features to ignore
+            ignoreFiles: ['**/normalize.css'], // an optional array of file globs to match against original source file path, to ignore
+            onFeatureUsage: function (usageInfo) {
+                console.log(usageInfo.message)
+            }
+        }),
+        //        require('stylelint'),
         require('cssnano')
     ]
 };
